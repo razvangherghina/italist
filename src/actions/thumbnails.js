53f6dae6ce7bbc6f8bac68a81036f997d5833035
Razvan Gherghina <gherghina.razvan@gmail.com>
2017-02-04 16:13:55 -0500

    Upload file

--- src/actions/thumbnails.js
+++ src/actions/thumbnails.js
@@ -2,10 +2,14 @@ import store from '../store';
 
 import convert from 'xml-to-json-promise';
 import factory from 'aws-api-gateway-client';
+
+import path from 'path';
+import {readAsDataURL} from 'promise-file-reader';
+
 const apiGatewayAddress = 'https://s5ntd5ns21.execute-api.eu-west-1.amazonaws.com/test';
 const s3Address = 'https://s3-eu-west-1.amazonaws.com/italist/';
 const api = factory.newClient({invokeUrl: apiGatewayAddress});
-
+const maxSize = 5 * 1024 * 1024;
 export const START_FETCH = 'START_FETCH';
 export const FETCH_ERROR = 'FETCH_ERROR';
 export const INSERT_LIST = 'INSERT_LIST';
@@ -43,55 +47,50 @@ export const fetchThumbnails = () => dispatch => {
         .catch(err => dispatch({type: FETCH_ERROR, payload: err}));
 };
 
-export const mockFetch = () => dispatch => {
-    dispatch({type: START_FETCH});
-    setTimeout(() => {
-        dispatch({
-            type: INSERT_LIST,
-            payload: {
-                t360: [
-                    'http://timelinethumbnailcreator.com/img/icon-brush-256.png', 'http://blog.room34.com/wp-content/uploads/underdog/logo.thumbnail.png'
-                ],
-                t120: [
-                    'https://cdn01.boxcdn.net/cdn/farfuture/QHbxX4l5tB7lGqFOmr8mpfgCToQjdC3CgzylCT7br' +
-                            'fQ/md5:307b50be8e0f182984d0c28bb81c20b8/sites/default/files/thumbnail-sample.png',
-                    'https://static1.squarespace.com/static/5515b9dfe4b0570dfe189823/t/56461426e4b04b' +
-                            '5d43f6c692/1447433254484/DTW-Website-Thumbnails-15.png'
-                ]
-            }
-        });
-    }, 1000);
-};
-
 const getThType = () => {
     const state = store.getState();
     return state.routing && state.routing.locationBeforeTransitions && state.routing.locationBeforeTransitions.pathname === '/big'
-        ? '360'
-        : '120';
+        ? 't360'
+        : 't120';
 };
+
+const rand = () => Math
+    .random()
+    .toString(36)
+    .substring(2) + Math
+    .random()
+    .toString(36)
+    .substring(2);
 
 export const upload = (files) => dispatch => {
 
     if (!files || !files.length) 
         return;
     const file = files[0];
-    const thType = getThType();
-    console.log('Uploading', file, getThType());
     dispatch({type: START_UPLOAD});
-    setTimeout(() => {
-        dispatch({type: ERROR_UPLOAD, payload: 'Generic error'});
-    }, 1000);
-    setTimeout(() => {
+    if (file.size > maxSize) 
+        return dispatch({type: ERROR_UPLOAD, payload: 'File too big (5Mb limit)!'});
+    const ext = path.extname(file.name);
+    const thType = getThType();
+    const key = rand();
+    const name = thType + '/' + key + ext;
+
+    console.log('Uploading', file, name);
+    readAsDataURL(file).then(buf => api.invokeApi({}, '/', 'POST', {}, {thType, name, buf})).then(res => {
+        const receivedKey = res && res.data;
+        if (!receivedKey) 
+            throw new Error('Upload error!');
         dispatch({
             type: OK_UPLOAD,
             payload: {
-                t120: thType === '120'
-                    ? file.name
+                t120: thType === 't120'
+                    ? s3Address + receivedKey
                     : null,
-                t360: thType === '360'
-                    ? file.name
+                t360: thType === 't360'
+                    ? s3Address + receivedKey
                     : null
             }
         });
-    }, 3000);
+    }).catch(err => dispatch({type: ERROR_UPLOAD, payload: err}));
+
 };
