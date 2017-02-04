1c9278f8c35f971b6e976b1e8f66aefd07d8159c
Razvan Gherghina <gherghina.razvan@gmail.com>
2017-02-04 06:53:39 -0500

    Refactored boilerplate

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import {Provider} from 'react-redux';
 import store from '../store';
 import router from '../router';
 
-
+// Injected Redux into main App Component
 const Main = () => (
   <Provider store={store}>
     {router}
